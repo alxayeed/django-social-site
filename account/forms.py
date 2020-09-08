5aaa0cbbfd5d6013d7c532de20ef7d736a0b136c
alxayeed <alxayeed@gmail.com>
2020-09-08 13:34:38 +0600

    custom login view created with User model

--- account/forms.py
+++ account/forms.py
@@ -4,4 +4,4 @@ from django import forms
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=20)
     # ensures html element type=password
-    password = forms.CharField(widgets=forms.PasswordInput)
+    password = forms.CharField(widget=forms.PasswordInput)
--- account/views.py
+++ account/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import LoginForm
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(data=request.POST)  # storing the HTML form
+        if form.is_valid():  # checks if all data are valid html input type
+            cd = form.cleaned_data  # getting data from form
+            # print(cd)
+            user = authenticate(
+                request, username=cd['username'], password=cd['password'])  # cross check the username and password with database User model. if matched, return the USer model, return none otherwise
+
+            # If User exists in Database(user model)
+            if user is not None:
+                # checks if user stratus is active or not
+                # but this is redundant
+                # if user is not active, authenticate returns None
+                # so, the else block will never execute if
+                if user.is_active:
+                    login(request, user)
+                    message = 'Login Successful'
+                else:
+                    message = 'Opps! account is disabled'
+            else:
+                message = 'Incorrect Username or Password'
+
+    else:
+        message = None
+        form = LoginForm()
+
+    return render(request, 'account/login.html', {'form': form, 'message': message})
