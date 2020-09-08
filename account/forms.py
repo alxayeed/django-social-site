382df84f673a8cfd9c24e6ab44e471b72c5432b8
alxayeed <alxayeed@gmail.com>
2020-09-08 12:13:28 +0600

    superuser and forms.py created

---
+++ account/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=20)
+    # ensures html element type=password
+    password = forms.CharField(widgets=forms.PasswordInput)
