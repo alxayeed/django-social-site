bf891e919dcad19a95216a3e2caace065693725e
alxayeed <alxayeed@gmail.com>
2020-09-14 19:29:58 +0600

    follow/unfollow ajax code added to the detail template. feature is working

--- common/decorators.py
+++ common/decorators.py
@@ -4,7 +4,7 @@ from django.http import HttpResponseBadRequest
 # custom decorator to check if incoming request is ajax or not
 def ajax_required(func):
     def wrapper(request, *args, **kwargs):
-        if not request.is_aja():
+        if not request.is_ajax():
             return HttpResponseBadRequest
         return func(request, *args, **kwargs)
     wrapper.__doc__ = func.__doc__
