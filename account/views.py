2e35edd788ba2c41124af1a21419ee27182ae08b
alxayeed <alxayeed@gmail.com>
2020-09-16 11:57:23 +0600

    template added for activity stream

--- account/views.py
+++ account/views.py
@@ -21,7 +21,7 @@ def dashboard(request):
                                                        flat=True)
     if following_ids:
         # if user's following others, retrieve only their actions
-        actions = actions.filter(user_id_in=following_ids)
+        actions = actions.filter(user__id__in=following_ids)
     actions = actions.select_related(
         'user', 'user__profile').prefetch_related('target')[:10]
     return render(request,
